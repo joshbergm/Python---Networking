f89a302d649907e8cb945dae6c395283fa87ab99
joshbergm <josh.bergman@outlook.com>
2024-06-28 10:48:02 +0200

    .

--- Fortinet/FortiManager/VDOM/01_create_mulitple_vdoms_from_csv.py
+++ Fortinet/FortiManager/VDOM/01_create_mulitple_vdoms_from_csv.py
@@ -18,6 +18,9 @@ username = input("Username: ")
 password = getpass.getpass(prompt="Password: ", stream=None)
 host = input("IP address: ")
 
+## FortiManager ADOM variable
+adom = "1PLATFORM"
+
 ## Get current username for file handling
 logged_in_user = getpass.getuser()
 
@@ -26,8 +29,7 @@ csv_delimiter = ';'
 
 ## Define file paths
 base_file_path = os.path.join("c:/Users/", logged_in_user, "Documents/Python-Networking/")
-vdom_input_csv_file = os.path.join("vdomcreationlist.csv")
-vdom_resource_input_csv_file = os.path.join("vdomresourcelist.csv")
+vdom_input_csv_file = os.path.join(base_file_path, "vdomcreationlist.csv")
 
 ## Ignore invalid SSL warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -62,6 +64,22 @@ print("Logged in successfully!")
 session_id_json_ouput = login_request.json()
 session_id = session_id_json_ouput.get('session')
 
+## Lock ADOM for VDOM creation
+payload_url_lock_adom = f'https://{host}/jsonrpc'
+json_api_adom_lock_body = {
+        "method": "exec",
+        "params": [
+            {
+                "url": f'/dvmdb/adom/{adom}/workspace/lock'
+            }
+        ],
+        "session": session_id,
+        "id": 1
+    }
+
+lock_adom_request = logout_request = requests.post(payload_url_lock_adom, json=json_api_adom_lock_body, headers=json_api_header, verify=False)
+print(f'Locked ADOM: {adom}')
+
 ## Read CSV file for VDOM creation
 with open(vdom_input_csv_file, 'r') as vdomlist:
     csv_reader = csv.reader(vdomlist, delimiter=csv_delimiter)
@@ -79,24 +97,23 @@ with open(vdom_input_csv_file, 'r') as vdomlist:
         
         ## JSON RPC API Data
         json_api_vdom_body = {
-            "method": "add",
-            "params": [
-                {
-                    "data": [
-                        {
-                            'name': vdom_name,
-                            'opmode': opmode,
-                            'status': ' ',
-                            'vdom_type': 'traffic',
-                            'comments': comments
-                        }
-                    ],
-                    "url": f'/dvmdb/device/{device_name}/vdom'
-                }
-            ],
-            "session": session_id,
-            "id": 1
-        }
+			"method": "add",
+			"params": [
+				{
+					"data": [
+						{
+							"comments": comments,
+							"name": vdom_name,
+							"opmode": opmode,
+							"vdom_type": "traffic",
+						}
+					],
+					"url": f"/dvmdb/adom/{adom}/device/{device_name}/vdom"
+				}
+			],
+			"session": session_id,
+			"id": 1
+		}
 
         ## Send request to create VDOM
         vdom_create_request = requests.post(payload_url, json=json_api_vdom_body, headers=json_api_header, verify=False)
@@ -107,6 +124,22 @@ with open(vdom_input_csv_file, 'r') as vdomlist:
 
 ## Let user know VDOM creation is done
 print("VDOM creation is done")
+
+## Unlock ADOM
+payload_url_unlock_adom = f'https://{host}/jsonrpc'
+json_api_adom_unlock_body = {
+        "method": "exec",
+        "params": [
+            {
+                "url": f'/dvmdb/adom/{adom}/workspace/unlock'
+            }
+        ],
+        "session": session_id,
+        "id": 1
+    }
+
+unlock_adom_request = logout_request = requests.post(payload_url_unlock_adom, json=json_api_adom_unlock_body, headers=json_api_header, verify=False)
+print(f'Unlocked ADOM: {adom}')
 
 ## Log out
 logout_body = {
