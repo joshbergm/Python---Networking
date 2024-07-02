6a3cfdc8898bac96f72184c1b5f3f319aa2235d0
joshbergm <josh.bergman@outlook.com>
2024-07-02 09:59:55 +0200

    .

--- Fortinet/FortiGate/Config/01_config_fortigate_from_csv.py
+++ Fortinet/FortiGate/Config/01_config_fortigate_from_csv.py
@@ -5,90 +5,95 @@ import getpass  # OS username handling
 import urllib3  # Suppress SSL warning
 
 # Device variables
-fortigate_api_token = ''
-fortigate_ip = ''
-fortigate_port = ''
+fortigate_api_token = '' ## Specify API token generated in FortiGate Administrators
+fortigate_ip = '' ## Specify FortiGate IP with HTTPS access
+fortigate_port = '' ## Specify FortiGate HTTPS port
 
 # Script variables
-csv_delimiter = ';'
-windowsusername = getpass.getuser()
+csv_delimiter = ';' ## Specify CSV delimiter for file handling
+windowsusername = getpass.getuser() ## Get logged in user for file handling
 apisession = requests.session()
 
 # Disable SSL verification
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) ## Disable invallid SSL warning
 apisession.verify = False
 
 # API header
 api_header = {
-    'Accept': 'application/json',
-    'Authorization': f'Bearer {fortigate_api_token}'
+    'Accept': 'application/json', ## Specify API format
+    'Authorization': f'Bearer {fortigate_api_token}' ## Specify Bearer token
 }
 
 # Default API prefix
 defaultapiurl = f'https://{fortigate_ip}:{fortigate_port}/api/v2'
 
-# Specific API prefixes
+## Specific API prefixes
+### Interface API
 interfaceapi = f'{defaultapiurl}/cmdb/system/interface'
-ipsecvpnapi = f'{defaultapiurl}/cmdb/vpn.ipsec'
-phase1_ipsecvpnapi = f'{ipsecvpnapi}/phase1'
-phase1interface_ipsecvpnapi = f'{ipsecvpnapi}/phase1-interface'
-phase2_ipsecvpnapi = f'{ipsecvpnapi}/phase2'
-phase2interface_ipsecvpnapi = f'{ipsecvpnapi}/phase2-interface'
+### Zone API
 zoneapi = f'{defaultapiurl}/cmdb/system/zone'
+### Address API
 addressapi = f'{defaultapiurl}/cmdb/firewall/address'
+### Policy API
 policyapi = f'{defaultapiurl}/cmdb/firewall/policy'
 
-# Functions
+## Functions
+### Interface function
 def create_interface(api_url, api_body, headers):
     response = apisession.post(api_url, json=api_body, headers=headers)
     return response
 
-def create_vpn(api_url, api_body, headers):
-    response = apisession.post(api_url, json=api_body, headers=headers)
-    return response
-
+### Zone function
 def create_zone(api_url, api_body, headers):
     response = apisession.post(api_url, json=api_body, headers=headers)
     return response
 
+### Address function
 def create_address(api_url, api_body, headers):
     response = apisession.post(api_url, json=api_body, headers=headers)
     return response
 
+### Policy function
 def create_policy(api_url, api_body, headers):
     response = apisession.post(api_url, json=api_body, headers=headers)
     return response
 
-# File locations
+## File locations
 config_file_path = os.path.join(f'c:/Users/{windowsusername}/Documents/Python-Networking/FortiGate/Configtool')
 
-csv_config = os.path.join(f'{config_file_path}/settings.txt')
+### Interface CSV file
 csv_interfaces = os.path.join(f'{config_file_path}/interfaces.csv')
-csv_vpns = os.path.join(f'{config_file_path}/vpns.csv')
+
+### Zone CSV file
 csv_zones = os.path.join(f'{config_file_path}/zones.csv')
+
+### Address CSV file
 csv_addresses = os.path.join(f'{config_file_path}/addresses.csv')
+
+### Policy CSV file
 csv_policies = os.path.join(f'{config_file_path}/policies.csv')
 
-# Modules
+## Modules
+### Interface module
 if input("Do you want to create interfaces? (y/n): ").lower() == 'y':
     with open(csv_interfaces, 'r') as interfaces:
         csv_reader = csv.reader(interfaces, delimiter=csv_delimiter)
         next(csv_reader, None)
 
         for row in csv_reader:
-            interface_name = row[0]
-            interface_vdom = row[1] if row[1] else 'root'
-            interface_member = row[2]
-            interface_vlanid = int(row[3])
-            interface_lacp_mode = row[4]
-            interface_lacp_speed = row[5]
-            interface_mode = row[6]
-            interface_ip = row[7]
-            interface_access = row[8]
-            interface_status = row[9]
-            interface_role = row[10]
-            interface_pppoe_username = row[11]
-            interface_pppoe_password = row[12]
+            interface_name = row[0] ## Interface name
+            interface_vdom = row[1] if row[1] else 'root' ## If value is blank, use root as default
+            interface_member = row[2] ## Interface member e.g. LACP bundle
+            interface_vlanid = int(row[3]) ##Interface VLAN id
+            interface_lacp_mode = row[4] ## active / passive
+            interface_lacp_speed = row[5] ## fast / slow
+            interface_mode = row[6] ## static / DHCP / PPPoE
+            interface_ip = row[7] ## e.g. 1.1.1.1/32
+            interface_access = row[8] ## PING HTTP HTTPS SSH
+            interface_status = row[9] ## up / down
+            interface_role = row[10] ## lan / wan / dmz / undefined
+            interface_pppoe_username = row[11] ## e.g. internet
+            interface_pppoe_password = row[12] ## e.g. internet
 
             interfaceapi_body = {
                 "name": interface_name,
@@ -105,33 +110,26 @@ if input("Do you want to create interfaces? (y/n): ").lower() == 'y':
                 "lacp-speed": interface_lacp_speed,
                 "role": interface_role
                 }
-
-            interfaceapi_request = create_interface(interfaceapi, interfaceapi_body, api_header)
+            
+            #### Send request and print status, throw error if status code is not 200
+            interfaceapi_request = create_interface(interfaceapi, interfaceapi_body, api_header) ## Call function named 'create_interface' and define values
             if interfaceapi_request.status_code == 200:
                 print(f'Interface creation for {interface_name} successfull')
             else:
-                print(f'Interface creation for {interface_name} failed, code: {interfaceapi_request.status_code}')
+                print(f'Interface creation for {interface_name} failed, code: {interfaceapi_request.status_code}') ## Print HTTP error status code
 
-if input("Do you want to create VPNs? (y/n): ").lower() == 'y':
-    with open(csv_vpns, 'r') as vpns:
-        csv_reader = csv.reader(vpns, delimiter=csv_delimiter)
-        next(csv_reader, None)
-
-        for row in csv_reader:
-            # Implement VPN creation similarly
-            pass
-
+### Zone module
 if input("Do you want to create zones? (y/n): ").lower() == 'y':
     with open(csv_zones, 'r') as zones:
         csv_reader = csv.reader(zones, delimiter=csv_delimiter)
         next(csv_reader, None)
 
         for row in csv_reader:
-            zone_name = row[0]
-            zone_description = row[1]
-            zone_intrazone = row[2]
-            zone_interface = row[3]
-            zone_vdom = row[4] if row[4] else 'root'
+            zone_name = row[0] ## Zone name
+            zone_description = row[1] ## Zone descriptions
+            zone_intrazone = row[2] ## Zone intrazone traffic allow / deny
+            zone_interface = row[3] ## Zone assigned interfaces
+            zone_vdom = row[4] if row[4] else 'root' ## If value is blank use root as default
 
             zoneapi_body = {
                 "params": {
@@ -149,12 +147,14 @@ if input("Do you want to create zones? (y/n): ").lower() == 'y':
                 }
             }
 
-            zoneapi_request = create_zone(zoneapi, zoneapi_body, api_header)
+            #### Send request and print status, throw error if status code is not 200
+            zoneapi_request = create_zone(zoneapi, zoneapi_body, api_header) ## Call function named 'create_zone' and define values
             if zoneapi_request.status_code == 200:
                 print(f'Zone creation for {zone_name} successfull')
             else:
-                print(f'Zone creation for {zone_name} failed, code: {zoneapi_request.status_code}')
+                print(f'Zone creation for {zone_name} failed, code: {zoneapi_request.status_code}') ## Print HTTP error status code
 
+### Address module
 if input("Do you want to create addresses? (y/n): ").lower() == 'y':
     with open(csv_addresses, 'r') as addresses:
         csv_reader = csv.reader(addresses, delimiter=csv_delimiter)
@@ -193,12 +193,14 @@ if input("Do you want to create addresses? (y/n): ").lower() == 'y':
                 }
             }
 
-            addressapi_request = create_address(addressapi, addressapi_body, api_header)
+            #### Send request and print status, throw error if status code is not 200
+            addressapi_request = create_address(addressapi, addressapi_body, api_header) ## Call function named 'create_address' and define values
             if addressapi_request.status_code == 200:
                 print(f'Address creation for {address_name} successfull')
             else:
-                print(f'Address creation for {address_name} failed, code: {addressapi_request.status_code}')
+                print(f'Address creation for {address_name} failed, code: {addressapi_request.status_code}') ## Print HTTP error status code
 
+### Policy module
 if input("Do you want to create policies? (y/n): ").lower() == 'y':
     with open(csv_policies, 'r') as policies:
         csv_reader = csv.reader(policies, delimiter=csv_delimiter)
@@ -257,8 +259,9 @@ if input("Do you want to create policies? (y/n): ").lower() == 'y':
                 }
             }
 
-            policyapi_request = create_policy(policyapi, policyapi_body, api_header)
+            #### Send request and print status, throw error if status code is not 200
+            policyapi_request = create_policy(policyapi, policyapi_body, api_header) ## Call function named 'create_policy' and define values
             if policyapi_request.status_code == 200:
                 print(f'Policy creation for {policy_name} successfull')
             else:
-                print(f'Policy creation for {policy_name} failed, code: {policyapi_request.status_code}')
+                print(f'Policy creation for {policy_name} failed, code: {policyapi_request.status_code}') ## Print HTTP error status code
